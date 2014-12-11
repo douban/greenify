4768bda4880b946d7a10c567c1e6646559b75c75
panmiaocai <panmiaocai@douban.com>
2014-12-11 13:53:07 +0800

    Support more greenified functions (Fix #7)
    readv, writev, pread, pwrite, recvfrom, sendto, recvmsg

--- include/libgreenify.h
+++ include/libgreenify.h
@@ -15,11 +15,23 @@ extern "C" {
 #endif
 
 int green_connect(int socket, const struct sockaddr *address, socklen_t address_len);
+
 ssize_t green_read(int fildes, void *buf, size_t nbyte);
 ssize_t green_write(int fildes, const void *buf, size_t nbyte);
+ssize_t green_pread(int fd, void *buf, size_t count, off_t offset);
+ssize_t green_pwrite(int fd, const void *buf, size_t count, off_t offset);
+ssize_t green_readv(int fd, const struct iovec *iov, int iovcnt);
+ssize_t green_writev(int fd, const struct iovec *iov, int iovcnt);
+
 ssize_t green_recv(int socket, void *buffer, size_t length, int flags);
 ssize_t green_send(int socket, const void *buffer, size_t length, int flags);
-ssize_t green_sendmsg(int socket, const struct msghdr* message, int flags);
+ssize_t green_recvmsg(int socket, struct msghdr *message, int flags);
+ssize_t green_sendmsg(int socket, const struct msghdr *message, int flags);
+ssize_t green_recvfrom(int sockfd, void *buf, size_t len, int flags,
+                       struct sockaddr *src_addr, socklen_t *addrlen);
+ssize_t green_sendto(int sockfd, const void *buf, size_t len, int flags,
+                     const struct sockaddr *dest_addr, socklen_t addrlen);
+
 int green_select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
 #ifndef NO_POLL
 int green_poll(struct pollfd *fds, nfds_t nfds, int timeout);

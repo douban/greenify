c9ac76718963a775dc18e41d898525d6b5f71ce9
everpcpc <git@everpcpc.com>
2020-08-21 14:33:54 +0800

    misc: bump version 0.3.3

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-import sys
 from glob import glob
 from setuptools import setup, Extension
 
-version = '0.3.2'
+version = '0.3.3'
+
 
 def readme():
     with open('README.rst') as f:
         return f.read()
+
 
 include_dirs = ["include"]
 sources = glob("*.pyx") + glob("src/*.c")
@@ -15,25 +16,24 @@ libraries = ["dl"]
 setup(
     name="greenify",
     version=version,
-    description = "Make C module compatible with gevent at runtime.",
+    description="Make C module compatible with gevent at runtime.",
     long_description=readme(),
     platforms=['Linux'],
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Cython',
-        'Programming Language :: C',
+        'Intended Audience :: Developers', 'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux', 'Programming Language :: Cython', 'Programming Language :: C',
         'Topic :: Software Development :: Libraries'
     ],
     author="Zhongbo Tian",
     author_email="tianzhongbo@douban.com",
     url="https://github.com/douban/greenify",
-    download_url = 'https://github.com/douban/greenify/archive/%s.tar.gz' % version,
+    download_url='https://github.com/douban/greenify/archive/%s.tar.gz' % version,
     setup_requires=['Cython >= 0.18'],
     install_requires=['gevent'],
-    ext_modules=[
-        Extension('greenify', sources, include_dirs=include_dirs,
-                  libraries=libraries)
-    ]
+    ext_modules=[Extension(
+        'greenify',
+        sources,
+        include_dirs=include_dirs,
+        libraries=libraries,
+    )],
 )
